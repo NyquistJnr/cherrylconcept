c85b20654fe922ab38187ecdfc2bcfb08aa85048
Nyquist Nwaukwa <nyquist.nwaukwa@gmail.com>
2025-09-17 19:56:04 +0100

    Wrapped up!

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,24 @@ export const metadata = {
   title: "Cherryl Concept",
   description:
     "Explore the full collection of premium hats, stylish caps, human hair wigs, and accessories at Cherryl Concept. Find your perfect style. Free shipping available!",
+
+  openGraph: {
+    title: "Cherryl Concept",
+    description:
+      "Discover our mission, values, and the story behind our brand.",
+    url: "https://cherrylconcept.com",
+    siteName: "Cherryl Concept",
+    images: [
+      {
+        url: "https://res.cloudinary.com/djs4r0egg/image/upload/v1751710851/products/jodsegtmaymkkicbuwnw.png",
+        width: 1200,
+        height: 630,
+        alt: "Cherryl Concept",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
